bb6589d9c5cf35c8974e9939416276ec0cc48a90
Pedro José Paulino Siqueira <pedro.siqueira45@etec.sp.gov.br>
2024-09-26 18:47:08 -0300

    Add files via upload

---
+++ exercicios_laco_de_repetisao/lista_do_for/do e for/src/Pares.java
@@ -0,0 +1,10 @@
+public class Pares {
+public static void main(String[] args) {
+	int i=0;
+	System.out.println("os numeros pares de 0 a 500 são :");
+	do {
+		System.out.println(i+";");
+		i+=2;
+	}while(i<=500);
+}
+}
